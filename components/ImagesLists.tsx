4a34cbe6e886699889b3619101ec18ac4df2dc52
Tanjid <tamjidahammad10@gmail.com>
2021-10-23 01:41:47 +0600

    only xd

--- components/ImagesLists.tsx
+++ components/ImagesLists.tsx
@@ -4,18 +4,20 @@ const ImagesLists = ({ posts }) => {
     <>
       <div>
         <h1>Images</h1>
-        {posts.map((value, i) => (
-          <div className="card bordered shadow-md hover:shadow-2xl" key={i}>
-            <figure>
-              {/*  eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src={value.imageAdress}
-                alt="blog"
-                className="object-cover h-64 w-full"
-              />
-            </figure>
-          </div>
-        ))}
+        <div className="grid grid-cols-3 gap-4">
+          {posts.map((value, i) => (
+            <div className="bordered shadow-md hover:shadow-2xl" key={i}>
+              <figure>
+                {/*  eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  src={value.imageAdress}
+                  alt="blog"
+                  className="object-cover h-64 w-full"
+                />
+              </figure>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
